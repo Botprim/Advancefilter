5ea86d4e0bf84049d88cb483108d9ab4a6c265f3
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2023-12-17 10:02:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -71,7 +71,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "❆ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ ❆", url="https://t.me/+ejpD6LYqsIRlZDg1"
+                    "❆ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ ❆", url="https://t.me/+Of41EvFpJ3FmODI9"
                 )
             ]
         ]
