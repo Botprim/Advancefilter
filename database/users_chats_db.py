31c7478fb87c60f3beb073d6648da8ff1f859ec4
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2025-03-04 00:03:48 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -10,7 +10,6 @@ class Database:
         self.col = self.db.users
         self.grp = self.db.groups
 
-
     def new_user(self, id, name):
         return dict(
             id = id,
@@ -20,7 +19,6 @@ class Database:
                 ban_reason="",
             ),
         )
-
 
     def new_group(self, id, title):
         return dict(
@@ -41,73 +39,57 @@ class Database:
         return bool(user)
     
     async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
-    
+        return await self.col.count_documents({})
+
     async def remove_ban(self, id):
-        ban_status = dict(
-            is_banned=False,
-            ban_reason=''
+        await self.col.update_one(
+            {'id': id},
+            {'$set': {'ban_status': {'is_banned': False, 'ban_reason': ''}}}
         )
-        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
     
     async def ban_user(self, user_id, ban_reason="No Reason"):
-        ban_status = dict(
-            is_banned=True,
-            ban_reason=ban_reason
+        await self.col.update_one(
+            {'id': user_id},
+            {'$set': {'ban_status': {'is_banned': True, 'ban_reason': ban_reason}}}
         )
-        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
 
     async def get_ban_status(self, id):
-        default = dict(
-            is_banned=False,
-            ban_reason=''
-        )
-        user = await self.col.find_one({'id':int(id)})
-        if not user:
-            return default
-        return user.get('ban_status', default)
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('ban_status', {'is_banned': False, 'ban_reason': ''}) if user else {'is_banned': False, 'ban_reason': ''}
 
     async def get_all_users(self):
         return self.col.find({})
-    
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
 
-
     async def get_banned(self):
-        users = self.col.find({'ban_status.is_banned': True})
-        chats = self.grp.find({'chat_status.is_disabled': True})
-        b_chats = [chat['id'] async for chat in chats]
-        b_users = [user['id'] async for user in users]
-        return b_users, b_chats
-    
+        users_cursor = self.col.find({'ban_status.is_banned': True})
+        chats_cursor = self.grp.find({'chat_status.is_disabled': True})
 
+        b_users = [user['id'] async for user in users_cursor]
+        b_chats = [chat['id'] async for chat in chats_cursor]
+
+        return b_users, b_chats  # ✅ Fixed MongoDB Async Issue
 
     async def add_chat(self, chat, title):
-        chat = self.new_group(chat, title)
-        await self.grp.insert_one(chat)
-    
+        await self.grp.insert_one(self.new_group(chat, title))
 
     async def get_chat(self, chat):
-        chat = await self.grp.find_one({'id':int(chat)})
-        return False if not chat else chat.get('chat_status')
-    
+        chat = await self.grp.find_one({'id': int(chat)})
+        return chat.get('chat_status', False) if chat else False
 
     async def re_enable_chat(self, id):
-        chat_status=dict(
-            is_disabled=False,
-            reason="",
-            )
-        await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
-        
+        await self.grp.update_one(
+            {'id': int(id)},
+            {'$set': {'chat_status': {'is_disabled': False, 'reason': ''}}}
+        )
+
     async def update_settings(self, id, settings):
         await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})
-        
-    
+
     async def get_settings(self, id):
-        default = {
+        default_settings = {
             'button': SINGLE_BUTTON,
             'botpm': P_TTI_SHOW_OFF,
             'file_secure': PROTECT_CONTENT,
@@ -124,31 +106,22 @@ class Database:
             'tutorial': TUTORIAL,
             'is_tutorial': IS_TUTORIAL
         }
-        chat = await self.grp.find_one({'id':int(id)})
-        if chat:
-            return chat.get('settings', default)
-        return default
-    
+        chat = await self.grp.find_one({'id': int(id)})
+        return chat.get('settings', default_settings) if chat else default_settings
 
     async def disable_chat(self, chat, reason="No Reason"):
-        chat_status=dict(
-            is_disabled=True,
-            reason=reason,
-            )
-        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
-    
+        await self.grp.update_one(
+            {'id': int(chat)},
+            {'$set': {'chat_status': {'is_disabled': True, 'reason': reason}}}
+        )
 
     async def total_chat_count(self):
-        count = await self.grp.count_documents({})
-        return count
-    
+        return await self.grp.count_documents({})
 
     async def get_all_chats(self):
         return self.grp.find({})
 
-
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
-
 
 db = Database(DATABASE_URI, DATABASE_NAME)
