421c3a8d12f1c4f91ba263d3d5cc853705e988a3
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2025-03-05 22:51:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,8 +44,11 @@ from lazybot.clients import initialize_clients
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 LazyPrincessBot.start()
-loop = asyncio.get_event_loop()
-
+try:
+    loop = asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
 async def Lazy_start():
     print('\n')
