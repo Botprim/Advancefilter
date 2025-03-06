8fc2ede130df033cde1649ea963cd0c2421b1c69
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2025-03-06 10:14:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,9 +95,9 @@ if 'DYNO' in environ:
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', 'weary-fania-friendship-d7fa46eb.koyeb.app')) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "http://weary-fania-friendship-d7fa46eb.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "http://weary-fania-friendship-d7fa46eb.koyeb.app/".format(FQDN, PORT)
+FQDN = str(getenv('FQDN', 'increasing-orsola-marvelbotz-781f228d.koyeb.app')) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+URL = "http://increasing-orsola-marvelbotz-781f228d.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "http://increasing-orsola-marvelbotz-781f228d.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "http://weary-fania-friendship-d7fa46eb.koyeb.app/".format(FQDN)
+    URL = "http://increasing-orsola-marvelbotz-781f228d.koyeb.app/".format(FQDN)
 else:
-    URL = "http://weary-fania-friendship-d7fa46eb.koyeb.app/".format(FQDN)
+    URL = "http://increasing-orsola-marvelbotz-781f228d.koyeb.app/".format(FQDN)
 
 
 
